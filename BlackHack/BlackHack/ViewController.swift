05ebdfebd0707218427b63c3af19edfc0a17afde
Nicolás Acosta <nicolas@sitiofull.com>
2020-10-06 19:00:10 -0300

    init

---
+++ BlackHack/BlackHack/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BlackHack
+//
+//  Created by Nicolás Acosta on 06/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
